129b14c56389e9e4b8a0083e05e63c08a076f18b
Lysander Pitu <lysanderpitu@yahoo.com>
2023-12-07 18:33:38 +0200

    added conversion

--- include/ClientConnection.h
+++ include/ClientConnection.h
@@ -5,18 +5,42 @@
 #include "../build/generated/proto.pb.h"
 #include "../build/generated/proto.grpc.pb.h"
 #include <string>
+#include <QByteArray>
+#include <QImage>
+#include "../include/ImageConvertor.h"
 
 class ImageServiceImpl final : public ImageService::Service {
 public:
     grpc::Status ProcessImage(grpc::ServerContext* context, const ImageRequest* request, ImageResponse* response) override {
-        std::cout << "Received image with ID: " << request->image_id() << std::endl;
+        QByteArray imageData = QByteArray::fromBase64(request->image_data().c_str());
 
+        // Check if image data is valid
+        if (imageData.isNull()) {
+            return grpc::Status::CANCELLED;
+        }
+
+        // Convert QImage to cv::Mat
+        QImage image;
+        image.loadFromData(reinterpret_cast<const uchar*>(imageData.constData()), imageData.size());
+
+        Server::ImageConvertor imageConvertor;
+        cv::Mat cvImage = imageConvertor.ConvertToCvMat(image);
+
+        // Testing purposes
+        cv::imwrite("output_image.jpg", cvImage);
+
+        // TODO: Analyze here
+
+
+
+        //TODO: Send Back data
         response->set_set_id("123");
         response->add_image_data("Processed Image Data 1");
         response->add_image_data("Processed Image Data 2");
 
         return grpc::Status::OK;
-    }
+}
+
 };
 
 void RunServer() {
